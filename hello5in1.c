434f4e0e337d58ed491d680b764553cad5ef9a74
David M. Syzdek <david@syzdek.net>
2009-07-10 02:15:49 -0800

    Updating comments in hello5in1.c

--- hello5in1.c
+++ hello5in1.c
@@ -1,9 +1,8 @@
-# /*
-#  *  Usage: cp   hello5in1.txt hello5in1.c   && gcc hello5in1.c   && ./a.out
-#  *         cp   hello5in1.txt hello5in1.m   && gcc hello5in1.m   && ./a.out
-#  *         cp   hello5in1.txt hello5in1.cpp && g++ hello5in1.cpp && ./a.out
-#  *         perl hello5in1.txt
-#  *         bash hello5in1.txt
+# /*  Usage: cp   hello5in1.c hello5in1.m   && gcc hello5in1.c   && ./a.out
+#  *         cp   hello5in1.c hello5in1.cpp && g++ hello5in1.cpp && ./a.out
+#  *         gcc  hello5in1.c && ./a.out
+#  *         perl hello5in1.c
+#  *         bash hello5in1.c
 #  */
 #include <stdio.h>
 #define true int main(void);int main(void) {
