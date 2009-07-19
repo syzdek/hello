c0596556ba8e60f8feeec665f58c5906d447ca83
David M. Syzdek <syzdek@blackmare.syzdek.net>
2009-07-18 19:54:43 -0800

    Adding vertical sine wave to hello-sine

--- hello-sine.c
+++ hello-sine.c
@@ -3,18 +3,23 @@
 #include <termios.h>
 #include <sys/ioctl.h>
 #include <math.h>
-double HelloWorld(double x, double w, int z)
+double HelloWorld(double x, double h, double w, int z)
 {
-   printf("\033[%i;%iH%c", z+1, (int)((sin(((x-((double)(z*2)))*(360.0/w))/57.3)*(w/2.0))+((w/2.0)+1.0)), __FUNCTION__[z]);
-   return(z++ < 10 ? HelloWorld(x, w, z) : 1.0);
+   printf("\033[%i;%iH%c",
+      (int)((sin(((x-((double)(z*2)))*(360.0/h))/57.3)*(h/2.0))+((h/2.0)+1.0)),
+      (int)((sin(((x-((double)(z*2)))*(360.0/w))/57.3)*(w/2.0))+((w/2.0)+1.0)),
+      __FUNCTION__[z]);
+   return(z++ < 10 ? HelloWorld(x, h, w, z) : 1.0);
 }
 int main(int argc, char * argv[])
 {
    double x = 0.0, b[512];
 #ifdef TIOCGSIZE
-   J1: x += HelloWorld(x, ioctl(0, TIOCGSIZE, &b) ? 80 : ((struct ttysize *)b)->ts_cols, 0);
+   J1: x += HelloWorld(x, ((struct ttysize *)b)->ts_rows,
+            ioctl(0,TIOCGSIZE,&b) ? 80 : ((struct ttysize *)b)->ts_cols, 0);
 #else
-   J1: x += HelloWorld(x, ioctl(0, TIOCGWINSZ, &b) ? 80 : ((struct winsize *)b)->ws_col, 0);
+   J1: x += HelloWorld(x, ((struct winsize *)b)->ws_row,
+            ioctl(0,TIOCGWINSZ,&b) ? 80 : ((struct winsize *)b)->ws_col, 0);
 #endif
    fflush(stdout);
    usleep(60000);
